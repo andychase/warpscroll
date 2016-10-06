2609ab7f1884230f20c8b8a78b7210a85deef0c3
Andy Chase <asperous2@gmail.com>
2016-10-06 13:55:47 -0700

    Add verbose name to travel planner app

---
+++ travel_planner/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'travel_planner.apps.TravelPlannerConfig'
--- travel_planner/apps.py
+++ travel_planner/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class TravelPlannerConfig(AppConfig):
     name = 'travel_planner'
+    verbose_name = 'Travel Planner'
