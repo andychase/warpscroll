ddec0fadd8124fe775282012032c440f827c2fe3
Andy Chase <asperous2@gmail.com>
2016-10-06 13:57:52 -0700

    Add Trip model to travel_planner
    This model has 5 basic fields as per the requirements: Owner, Destination, StartDate, EndDate, Comment

--- travel_planner/models.py
+++ travel_planner/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Trip(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    destination = models.CharField(max_length=200)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return self.destination
