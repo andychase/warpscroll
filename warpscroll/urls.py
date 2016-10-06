65d29d4f3a8f3a267e33002723f0dd3bfdd2796e
Andy Chase <asperous2@gmail.com>
2016-10-06 13:32:41 -0700

    Install django rest framework

--- warpscroll/urls.py
+++ warpscroll/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+
+from travel_planner import api_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(api_views.router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
