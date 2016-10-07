e1a00c187ec6d69b476a190e7a4798748dd27961
Andy Chase <asperous2@gmail.com>
2016-10-07 16:23:49 -0700

    Add "days left" for upcoming trips

--- travel_planner/views.py
+++ travel_planner/views.py
@@ -1,14 +1,29 @@
-from datetime import datetime
-
+from datetime import date, timedelta
+from dateutil.relativedelta import relativedelta
 from django.shortcuts import render
+from django.template.loader_tags import register
 
 from travel_planner.models import Trip
 
 
-def get_upcoming_trips(user):
-    for trip in Trip.objects.filter(owner=user, start_date__gt=datetime.now()).order_by("-start_date"):
-        # trip.days_left = datetime.now() - trip.start_date
-        yield trip
+def _days_left(start_date):
+    positive = start_date - date.today() > timedelta()
+    if positive:
+        relative_date = relativedelta(start_date, date.today())
+        years, months, days = relative_date.years, relative_date.months, relative_date.days
+        for caption, item in (("year", years), ("month", months), ("day", days)):
+            if item:
+                yield "{} {}".format(item, caption)
+                if item > 1:
+                    yield "s"
+                yield " "
+
+
+def days_left(start_date):
+    return "".join(_days_left(start_date))
+
+
+register.filter('days_left', days_left)
 
 
 def home(request):
@@ -18,14 +33,17 @@ def home(request):
         {
             "current_trips": Trip.objects.filter(
                 owner=request.user,
-                start_date__lt=datetime.now(),
-                end_date__gt=datetime.now(),
+                start_date__lte=date.today(),
+                end_date__gte=date.today(),
             ),
-            "upcoming_trips": list(get_upcoming_trips(request.user)),
+            "upcoming_trips": Trip.objects.filter(
+                owner=request.user,
+                start_date__gt=date.today()
+            ),
             "past_trips": Trip.objects.filter(
                 owner=request.user,
-                end_date__lt=datetime.now(),
+                end_date__lt=date.today(),
             ).order_by("-start_date"),
-            "current_day": datetime.now()
+            "current_day": date.today()
         }
     )
