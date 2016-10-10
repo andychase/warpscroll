994438478c95e2e2bb6c97c605a45621fca38f2f
Andy Chase <asperous2@gmail.com>
2016-10-09 20:17:20 -0700

    Fix white noise configuration

--- warpscroll/production.py
+++ warpscroll/production.py
@@ -1,11 +1,6 @@
 from warpscroll.settings import *
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 
 import dj_database_url
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
-
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
